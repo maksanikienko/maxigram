f270b20cfd2e4ff966dbf5f3ae5776b403f9dcc3
maxim.anichienco <maxim.anichienco@ctif.gov.md>
2026-05-07 10:08:34 +0300

    Maxigram WIP - fix echo.js

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -7,10 +7,14 @@ window.Echo = new Echo({
     broadcaster: 'reverb',
     key: import.meta.env.VITE_REVERB_APP_KEY,
     wsHost: import.meta.env.VITE_REVERB_HOST,
-    wsPort: '8080',
-    forceTLS: false,
-    disableStats: true,
+    wsPort: import.meta.env.VITE_REVERB_PORT,
+    wssPort: import.meta.env.VITE_REVERB_PORT,
+
+    forceTLS: import.meta.env.VITE_REVERB_SCHEME === 'https',
     enableTransports: ['ws'],
+
+    disableStats: true,
+    authEndpoint: '/broadcasting/auth',
 });
 console.log(import.meta.env.VITE_REVERB_HOST)
 console.log(import.meta.env.VITE_REVERB_PORT)
