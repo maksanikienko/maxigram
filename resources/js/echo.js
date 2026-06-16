b83f8c3a6fda8331ff22b345c26e109eda0946a7
maxim.anichienco <maxim.anichienco@ctif.gov.md>
2026-06-16 11:51:30 +0300

    Maxigram WIP

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -3,16 +3,14 @@ import Echo from 'laravel-echo';
 import Pusher from 'pusher-js';
 window.Pusher = Pusher;
 
-const isTLS = import.meta.env.VITE_REVERB_SCHEME === 'https';
-
 window.Echo = new Echo({
     broadcaster: 'reverb',
     key: import.meta.env.VITE_REVERB_APP_KEY,
     wsHost: import.meta.env.VITE_REVERB_HOST,
     wsPort: import.meta.env.VITE_REVERB_WS_PORT ?? 8080,
     wssPort: import.meta.env.VITE_REVERB_WSS_PORT ?? 443,
-    forceTLS: isTLS,
-    enabledTransports: isTLS ? ['wss'] : ['ws'],
+    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'http') === 'https',
+    enabledTransports: ['ws', 'wss'],
     disableStats: true,
     authEndpoint: '/broadcasting/auth',
 });
